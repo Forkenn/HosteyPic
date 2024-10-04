b1513f40127074a4788702edbd1f3aaed202ab94
Forkenn <forkenn@example.com>
2024-10-04 20:29:52 +0300

    Nginx added

--- server/hosteypic_server/main.py
+++ server/hosteypic_server/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .test.router import router as router_test
 # from .auth.router import router as router_auth
 
 app = FastAPI(title='HosteyPic', root_path='/api')
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(router_test)
 # app.include_router(router_auth)
