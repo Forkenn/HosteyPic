b279a53fff7b2a33afb8d50661300f5e6ae2768c
Forkenn <forkenn@example.com>
2024-11-14 16:10:25 +0300

    Add validation on profile urls

--- server/hosteypic_server/users/schemas.py
+++ server/hosteypic_server/users/schemas.py
@@ -1,6 +1,8 @@
+import re
+
 from typing import List, Optional
 
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
 
 from hosteypic_server.auth.schemas import SUsername
 
@@ -30,11 +32,51 @@ class SMultiUserRead(BaseModel):
     items: List[SUserRead]
 
 class SUserEdit(BaseModel):
-    about_me: str | None
-    vk_link: str | None
-    ok_link: str | None
-    github_link: str | None
-    gitlab_link: str | None
+    about_me: Optional[str] = Field(default=None, max_length=140)
+    vk_link: Optional[str] = Field(default=None, max_length=140)
+    ok_link: Optional[str] = Field(default=None, max_length=140)
+    github_link: Optional[str] = Field(default=None, max_length=140)
+    gitlab_link: Optional[str] = Field(default=None, max_length=140)
+
+    @field_validator("vk_link")
+    @classmethod
+    def validate_vk_link(cls, vk_link: str) -> str:
+        re_for_usrname: re.Pattern[str] = re.compile(
+            r"^(https?://vk\.com/)([a-zA-Z0-9._]{4,30}|id\d{1,28})$"
+        )
+        if not re_for_usrname.match(vk_link):
+            raise ValueError("VK link does not meet the requirements")
+        return vk_link
+    
+    @field_validator("ok_link")
+    @classmethod
+    def validate_ok_link(cls, ok_link: str) -> str:
+        re_for_usrname: re.Pattern[str] = re.compile(
+            r"^(https?://ok\.ru/profile/)(\d{1,30})$"
+        )
+        if not re_for_usrname.match(ok_link):
+            raise ValueError("OK link does not meet the requirements")
+        return ok_link
+    
+    @field_validator("github_link")
+    @classmethod
+    def validate_github_link(cls, github_link: str) -> str:
+        re_for_usrname: re.Pattern[str] = re.compile(
+            r"^(https?://github\.com/)([a-zA-Z0-9](?:-?[a-zA-Z0-9]){0,38})$"
+        )
+        if not re_for_usrname.match(github_link):
+            raise ValueError("GitHub link does not meet the requirements")
+        return github_link
+    
+    @field_validator("gitlab_link")
+    @classmethod
+    def validate_gitlab_link(cls, ok_link: str) -> str:
+        re_for_usrname: re.Pattern[str] = re.compile(
+            r"^(https?://gitlab\.com/)([a-zA-Z0-9](?:[._-]?[a-zA-Z0-9]){0,29})$"
+        )
+        if not re_for_usrname.match(ok_link):
+            raise ValueError("GitLab link does not meet the requirements")
+        return ok_link
 
 class SUserUsernameEdit(SUsername):
     pass
