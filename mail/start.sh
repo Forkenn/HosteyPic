d41191ec70d40b6b0683ad36a68784d0549b8337
Forkenn <forkenn@example.com>
2024-11-23 16:20:20 +0300

    Fix dkim on mails

--- mail/start.sh
+++ mail/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-/usr/sbin/opendkim
+/usr/sbin/rsyslogd
+/usr/sbin/opendkim -x /etc/opendkim/opendkim.conf
 /usr/sbin/postfix start-fg
+
+sleep 3
+
+touch /var/log/mail.log; tail -f /var/log/mail.log
